27341be9b66c26bb26f7504b3cd0677be1e094b5
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-07-22 15:15:32 +0200

    Add cookie based session store to backend

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = module.exports = express();
 
-var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 
 app.set('json spaces', 2);
 app.set('x-powered-by', false);
 app.set('etag', false);
 
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: process.env.SECRET || 'this is not secret',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use('/auth', require('./controllers/auth'));
 app.use('/api/v1', require('./controllers/turbasen'));
