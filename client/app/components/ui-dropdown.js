67c251f6e7025d827fbe53daf19ae46bbaea4eb6
Håvard Ranum <havard.ranum@me.com>
2015-08-13 07:30:09 +0200

    Add support for search to dropdown component

--- client/app/components/ui-dropdown.js
+++ client/app/components/ui-dropdown.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
 
   tagName: 'select',
   classNames: ['ui', 'dropdown', 'fluid'],
+  classNameBindings: ['search'],
 
   content: null,
   optionValuePath: null,
