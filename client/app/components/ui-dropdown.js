153c4feaaa49493998ad76ecb00e1e0d8fa2d64d
Håvard Ranum <havard.ranum@me.com>
2015-08-13 08:27:48 +0200

    Change markup to semantic ui specific & add support for multiple select fields

--- client/app/components/ui-dropdown.js
+++ client/app/components/ui-dropdown.js
@@ -3,44 +3,54 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
 
-  tagName: 'select',
-  classNames: ['ui', 'dropdown', 'fluid'],
-  classNameBindings: ['search'],
+  tagName: 'div',
+  classNames: ['ui', 'selection', 'dropdown', 'fluid'],
+  classNameBindings: ['multiple', 'search'],
 
   content: null,
   optionValuePath: null,
   optionLabelPath: null,
+  allowAdditions: null,
 
-  bindAttributes: ['optionValuePath', 'optionLabelPath', 'content'],
+  bindAttributes: ['optionValuePath', 'optionLabelPath', 'content', 'allowAdditions'],
 
   actions: {},
 
   options: function () {
     var optionLabelPath = this.get('optionLabelPath');
     var optionValuePath = this.get('optionValuePath');
+    var content = this.get('content');
 
-    return this.get('content').map(function (item, index, list) {
+    return content ? content.map(function (item, index, list) {
       return {
         value: optionValuePath ? item.get(optionValuePath) : item,
         label: optionLabelPath ? item.get(optionLabelPath) : item
       };
-    }, this);
+    }, this) : [];
 
   }.property('content.@each'),
 
   setup: function () {
     this.$().dropdown({
+      allowAdditions: this.allowAdditions,
       onChange: Ember.run.bind(this, this.onChange)
     });
   }.on('didInsertElement'),
 
   onChange: function (value, text, $choice) {
-
     if (this.optionValuePath) {
       value = this.get('content').findBy(this.optionValuePath, value);
     }
 
-    this.set('value', value);
+    if (this.get('multiple')) {
+      var currentValue = this.get('value') || Ember.A([]);
+      currentValue.pushObject(value);
+      this.set('value', currentValue);
+
+    } else {
+      this.set('value', value);
+    }
+
   }
 
 });
