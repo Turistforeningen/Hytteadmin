dec59259a4271f25c8f088b942ed896f42c45ecf
Håvard Ranum <havard.ranum@me.com>
2015-08-14 10:18:18 +0200

    Add necessary fields to photo model

--- client/app/models/photo.js
+++ client/app/models/photo.js
@@ -1,5 +1,21 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+
+  beskrivelse: DS.attr('string'),
+  // checksum: DS.attr('string'), // TODO: Add support for readonly as this is set by NTB
+  // endret: DS.attr('string'), // TODO: Add support for readonly as this is set by NTB
+  eier: DS.attr('object'),
+  fotograf: DS.attr('object'),
+  geojson: DS.attr('object'),
+  grupper: DS.attr('array'),
+  img: DS.attr('object'),
+  lisens: DS.attr('string', {default: 'CC BY-SA 4.0'}),
+  navn: DS.attr('string'),
+  navngiving: DS.attr('string'),
+  privat: DS.attr('object'),
+  status: DS.attr('string'),
+  tags: DS.attr('array'),
+  // tilbyder: DS.attr('string') // TODO: Add support for readonly as this is set by NTB
+
 });
