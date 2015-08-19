ae9c42c279b812f23173d32ff0029c7765bd1abd
Håvard Ranum <havard.ranum@me.com>
2015-08-19 14:04:05 +0200

    Might not need these hooks anymore

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -5,30 +5,42 @@ export default DS.RESTAdapter.extend({
   namespace: 'api/v1',
   pathForType: function(type) {
     return Ember.String.underscore(type);
-  },
-
-  parseErrorResponse: function (responseText) {
-    var json = responseText;
-    console.log('parseErrorResponse');
-    try {
-      json = Ember.$.parseJSON(responseText);
-    } catch (e) {}
-
-    return json;
-  },
-
-  normalizeErrorResponse: function (status, headers, payload) {
-    console.log('normalizeErrorResponse');
-    if (payload && typeof payload === "object" && payload.errors) {
-      return payload.errors;
-
-    } else {
-      return [{
-        status: "" + status,
-        title: "The backend responded with an error",
-        details: "" + payload.message
-      }];
-    }
   }
+
+  // handleResponse: function(status, headers, payload) {
+  //   if (this.isSuccess(status, headers, payload)) {
+  //     return payload;
+  //   } else if (this.isInvalid(status, headers, payload)) {
+  //     return new InvalidError(payload.errors);
+  //   }
+
+  //   let errors = this.normalizeErrorResponse(status, headers, payload);
+
+  //   return new AdapterError(errors);
+  // },
+
+  // parseErrorResponse: function (responseText) {
+  //   var json = responseText;
+  //   console.log('parseErrorResponse');
+  //   try {
+  //     json = Ember.$.parseJSON(responseText);
+  //   } catch (e) {}
+
+  //   return json;
+  // },
+
+  // normalizeErrorResponse: function (status, headers, payload) {
+  //   console.log('normalizeErrorResponse');
+  //   if (payload && typeof payload === "object" && payload.errors) {
+  //     return payload.errors;
+
+  //   } else {
+  //     return [{
+  //       status: "" + status,
+  //       title: "The backend responded with an error",
+  //       details: "" + payload.message
+  //     }];
+  //   }
+  // }
 
 });
