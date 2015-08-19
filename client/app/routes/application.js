60c709119430feae37cce4d5254276463ad7e2ce
Håvard Ranum <havard.ranum@me.com>
2015-08-19 14:24:09 +0200

    Check for user in application route

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -5,5 +5,16 @@ export default Ember.Route.extend({
   setupController: function(controller, model) {
     var groups = this.store.findAll('group');
     this.controllerFor('groups').set('model', groups);
+
+    var users = this.store.findAll('user').then(function (users) {
+      console.log('users', users);
+
+    }).catch(function (err) {
+      if (err.authenticated === false) {
+        // TODO: Redirect to login
+      }
+
+    });
+
   }
 });
