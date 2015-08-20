fbe4e5b3be7be3a0b13edfc657816e6b41da10f8
Håvard Ranum <havard.ranum@me.com>
2015-08-20 15:41:22 +0200

    Limiting ckeditor funcitonality, custom button setup

--- client/app/components/ck-editor.js
+++ client/app/components/ck-editor.js
@@ -9,7 +9,25 @@ export default Ember.Component.extend({
 
   didInsertElement() {
     let textarea = this.element.querySelector('.editor');
-    let editor = this._editor = CKEDITOR.replace(textarea);
+    let editor = this._editor = CKEDITOR.replace(textarea, {
+      language: 'no',
+      // Remove elements path in footer
+      removePlugins: 'elementspath',
+      // Whether to use HTML entities in the output.
+      entities: false,
+
+      toolbar: [
+        {name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', '-', 'RemoveFormat']},
+        {name: 'styles', items: ['Format']},
+        {name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Blockquote']},
+        {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
+        {name: 'insert', items: ['Table']}
+      ],
+
+      format_tags: 'p;h2;h3'
+
+    });
+
     editor.on('change', (e) => {
       this.set('value', e.editor.getData());
     });
