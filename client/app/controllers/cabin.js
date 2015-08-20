0309c936db5a92415ec6dbd74597cedfbf362e26
Håvard Ranum <havard.ranum@me.com>
2015-08-20 09:15:58 +0200

    Set kollektiv object to undefined if toggled off

--- client/app/controllers/cabin.js
+++ client/app/controllers/cabin.js
@@ -37,8 +37,13 @@ export default Ember.Controller.extend({
       if (value) {
         this.set('tilkomst_kollektiv_enabled', true);
 
+        if (typeof this.get('model.tilkomst.kollektiv') === 'undefined') {
+          this.set('model.tilkomst.kollektiv', {'sommer': undefined, 'vinter': undefined});
+        }
+
       } else {
-        this.set('model.tilkomst.kollektiv', {'sommer': undefined, 'vinter': undefined});
+        this.set('model.tilkomst.kollektiv', undefined);
+        this.set('tilkomst_kollektiv_enabled', false);
       }
     }
   },
