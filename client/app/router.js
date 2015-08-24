788d91843a9f0a042bf09d187d14f8bb4e132d28
Håvard Ranum <havard.ranum@me.com>
2015-08-24 10:24:21 +0200

    DEPRECATION: this.resource() is deprecated. Use this.route('name', { resetNamespace: true }, function () {}) instead.

--- client/app/router.js
+++ client/app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function () {
-  this.resource('cabins', {path: '/hytter'}, function () {
+  this.route('cabins', {path: '/hytter', resetNamespace: true}, function () {
     this.route('edit', {path: '/:id'});
   });
 });
