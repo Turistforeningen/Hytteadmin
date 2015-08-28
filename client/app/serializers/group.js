536e0b3ed9cd9fde6203ed2877078d629de10b24
Håvard Ranum <havard.ranum@me.com>
2015-08-28 17:25:58 +0200

    Add serializer for group to rename kontaktinfo object tittel property to type

---
+++ client/app/serializers/group.js
@@ -0,0 +1,34 @@
+import DS from 'ember-data';
+
+import ApplicationSerializer from '../serializers/application';
+
+export default ApplicationSerializer.extend({
+
+  normalize: function (modelClass, resourceHash, prop) {
+    var normalizedHash = resourceHash;
+
+    if (resourceHash.kontaktinfo) {
+      for (var i = 0; i < resourceHash.kontaktinfo.length; i++) {
+        resourceHash.kontaktinfo[i]['type'] = resourceHash.kontaktinfo[i]['tittel'];
+        delete resourceHash.kontaktinfo[i]['tittel'];
+      }
+    }
+
+    return this._super(modelClass, normalizedHash, prop);
+  },
+
+  serialize: function(snapshot, options) {
+    var json = this._super(snapshot, options);
+
+    // Rename kontaktinfo object property `type` back to `tittel`
+    if (json.kontaktinfo) {
+      for (var i = 0; i < json.kontaktinfo.length; i++) {
+        json.kontaktinfo[i]['tittel'] = json.kontaktinfo[i]['type'];
+        delete json.json[i]['type'];
+      }
+    }
+
+    return json;
+  }
+
+});
