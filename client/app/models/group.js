f80bf405f645cadeee4840fe68a23675915e51f9
Håvard Ranum <havard.ranum@me.com>
2015-08-31 08:49:26 +0200

    Add fields to group

--- client/app/models/group.js
+++ client/app/models/group.js
@@ -1,5 +1,27 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  navn: DS.attr('string')
+  // checksum: DS.attr('string'), // TODO: Add support for readonly as this is set by NTB
+  // endret: DS.attr('string'), // TODO: Add support for readonly as this is set by NTB
+  foreldregruppe: DS.attr('string'), // DS.belongsTo('group', {async: true}),
+  grupper: DS.attr('array'), // DS.hasMany('group', {async: true}),
+  kontaktinfo: DS.attr('array', {defaultValue: [{'type': 'Primærkontakt'}]}),
+  kontaktinfo_primærkontakt: Ember.computed('kontaktinfo.[]', 'kontaktinfo.@each.type', {
+    get: function () {
+      var kontaktinfo = this.get('kontaktinfo');
+      if (kontaktinfo) {
+        return this.get('kontaktinfo').findBy('type', 'Primærkontakt');
+      }
+    }
+  }),
+  lisens: DS.attr('string', {defaultValue: 'CC BY-SA 4.0'}),
+  logo: DS.attr('string'),
+  navn: DS.attr('string'),
+  navngiving: DS.attr('string'),
+  privat: DS.attr('object'),
+  status: DS.attr('string'),
+  tags: DS.attr('array'),
+  tilbyder: DS.attr('string'),
+  url: DS.attr('string')
 });
