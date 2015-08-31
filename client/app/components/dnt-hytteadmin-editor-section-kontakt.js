d01d51c6e3d8d7dcc7a72384277bee95db813474
Håvard Ranum <havard.ranum@me.com>
2015-08-31 08:53:13 +0200

    Add action for setting kontaktinfo by group id

--- client/app/components/dnt-hytteadmin-editor-section-kontakt.js
+++ client/app/components/dnt-hytteadmin-editor-section-kontakt.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    setKontaktinfoGruppeById: function (id) {
+      var model = this.get('model');
+      if (typeof model.setKontaktinfoGruppeById) {
+        model.setKontaktinfoGruppeById(id);
+      }
+    }
+  }
+
 });
