641db2b243457e136f3c7a76c29ae94f4f9c5635
Håvard Ranum <havard.ranum@me.com>
2015-09-02 16:25:59 +0200

    Do not load groups on app init, not necessary

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -15,10 +15,5 @@ export default Ember.Route.extend({
     // console.log('route:application:beforeModel');
     this.controllerFor('session').set('previousTransition', transition);
     this.controllerFor('session').send('validate');
-  },
-
-  setupController: function(controller, model) {
-    var groups = this.store.findAll('group');
-    this.controllerFor('groups').set('model', groups);
   }
 });
