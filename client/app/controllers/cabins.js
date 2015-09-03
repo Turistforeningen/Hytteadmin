8473d6ac06795898c36cb4d3bd1ceef7655c1cd1
Håvard Ranum <havard.ranum@me.com>
2015-09-03 12:47:53 +0200

    Added search input to index page

--- client/app/controllers/cabins.js
+++ client/app/controllers/cabins.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isLoading: true,
+  params: {},
+
+  actions: {
+    search: function () {
+      var params = this.get('params');
+      Ember.set(params, 'navn', '~' + this.get('searchTerm'));
+      this.store.unloadAll('cabin');
+      this.set('isLoading', true);
+      this.store.query('cabin', params).then((cabins) => {
+        this.set('model', cabins);
+      });
+    }
+  },
+
+  onModelChange: function () {
+    this.set('isLoading', false);
+  }.observes('model')
 });
