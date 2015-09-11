96608b01f660b91f1d51f83ce56fb4374a8d58e1
Håvard Ranum <havard.ranum@me.com>
2015-09-11 15:07:42 +0200

    If validationRules is passed to ui-form component, apply validation to the form on input fields blur

--- client/app/components/ui-form.js
+++ client/app/components/ui-form.js
@@ -2,5 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   element: 'form',
-  classNames: ['ui form']
+  classNames: ['ui form'],
+
+  attributeBindings: ['validationRules'],
+  validationRules: null,
+
+  setup: function () {
+    let validationRules = this.get('validationRules');
+
+    if (validationRules) {
+      this.$().form({
+        fields: validationRules,
+        inline: true,
+        on: 'blur'
+      });
+    }
+  }.on('didInsertElement')
+
 });
