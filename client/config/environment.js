99298811ac2006511d55eb91346ef4783271a9c7
Håvard Ranum <havard.ranum@me.com>
2015-09-11 15:21:34 +0200

    Adjust raven config to avoid console errors

--- client/config/environment.js
+++ client/config/environment.js
@@ -31,8 +31,8 @@ module.exports = function(environment) {
       cdn: '//cdn.ravenjs.com',
       dsn: 'https://1d5112a03f0941278212feeea3a98c75@sentry.app.dnt.no/16',
       version: '1.1.19',
-      whitelistUrls: ['localhost:4200', 'hytte.app.dnt.privat'],
-      development: true // Set to true, to disable while developing
+      whitelistUrls: [/hytte\.app\.dnt\.no/], // Use regex if only setting domain
+      development: false // Set to true, to disable while developing
     }
   };
 
