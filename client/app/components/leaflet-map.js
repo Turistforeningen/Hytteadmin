4c10ac2c38e20be4f498eec2e368110afc66e345
Håvard Ranum <havard.ranum@me.com>
2015-09-17 13:01:31 +0200

    Added new helper methods to leaflet-map component

--- client/app/components/leaflet-map.js
+++ client/app/components/leaflet-map.js
@@ -41,10 +41,37 @@ export default Ember.Component.extend({
 
   }.on('didInsertElement'),
 
+  validateCoords: function (input) {
+    return (Ember.typeOf(input) === 'array' && input.length === 2 && input[0] > 0 && input[1] > 0);
+  },
+
+  latLngToCoords: function (latLng) {
+    return latLng.reverse();
+  },
+
+  coordsToLatLng: function (coords) {
+    return coords.reverse();
+  },
+
+  toLatLng: function (input) {
+    let latLng;
+
+    if (input && input.type === 'Point') {
+      // Assume center is a GeoJSON object with long & lat in coordinates array
+      input = input.coordinates.reverse();
+    }
+
+    if (this.validateCoords(input)) {
+      latLng = input;
+    }
+
+    return latLng;
+  },
+
   initMarker: function () {
     var markerPosition = this.get('marker');
 
-    if (markerPosition) {
+    if (this.validateCoords(markerPosition)) {
       var marker = L.marker(markerPosition);
       marker.addTo(this.map);
       this.set('_marker', marker);
@@ -53,6 +80,11 @@ export default Ember.Component.extend({
 
   updateMarker: function () {
     var marker = this.get('_marker');
+    var markerLatLng = this.get('marker');
+
+    if (!this.validateCoords(markerLatLng)) {
+      return;
+    }
 
     if (marker) {
       marker.setLatLng(this.get('marker'));
