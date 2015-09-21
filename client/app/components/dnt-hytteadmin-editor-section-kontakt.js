2458cbdc0f96478509fb65f95137005aeb9baaea
Håvard Ranum <havard.ranum@me.com>
2015-09-21 14:15:30 +0200

    Added missing comparison from conditional

--- client/app/components/dnt-hytteadmin-editor-section-kontakt.js
+++ client/app/components/dnt-hytteadmin-editor-section-kontakt.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   actions: {
     setKontaktinfoGruppeById: function (id) {
       var model = this.get('model');
-      if (typeof model.setKontaktinfoGruppeById) {
+      if (Ember.typeOf(model.setKontaktinfoGruppeById) === 'function') {
         model.setKontaktinfoGruppeById(id);
       }
     }
