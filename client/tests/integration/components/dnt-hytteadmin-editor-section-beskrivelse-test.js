d1115f8b5a9126506737df444b26c9d34368c430
Håvard Ranum <havard.ranum@me.com>
2015-09-22 16:15:59 +0200

    Moved beskrivelse section to separate component

---
+++ client/tests/integration/components/dnt-hytteadmin-editor-section-beskrivelse-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('dnt-hytteadmin-editor-section-beskrivelse', 'Integration | Component | dnt hytteadmin editor section beskrivelse', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{dnt-hytteadmin-editor-section-beskrivelse}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#dnt-hytteadmin-editor-section-beskrivelse}}
+      template block text
+    {{/dnt-hytteadmin-editor-section-beskrivelse}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
