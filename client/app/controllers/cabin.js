3f8e9c938d7170f359c7bfb33e821d35d3a6772e
Håvard Ranum <havard.ranum@me.com>
2015-09-23 14:51:22 +0200

    Moved method to component where template code has been moved

--- client/app/controllers/cabin.js
+++ client/app/controllers/cabin.js
@@ -110,20 +110,6 @@ export default Ember.Controller.extend({
       this.set('model.privat.kun_bestilling', kunBestilling);
     },
 
-    toggleTilkomstKollektiv: function (value) {
-      if (value) {
-        this.set('tilkomst_kollektiv_enabled', true);
-
-        if (typeof this.get('model.tilkomst.kollektiv') === 'undefined') {
-          this.set('model.tilkomst.kollektiv', {'sommer': undefined, 'vinter': undefined});
-        }
-
-      } else {
-        this.set('model.tilkomst.kollektiv', undefined);
-        this.set('tilkomst_kollektiv_enabled', false);
-      }
-    },
-
     setKontaktinformasjonISesongFromJuridiskEier: function () {
       console.log('setKontaktinformasjonISesongFromJuridiskEier', arguments);
     },
