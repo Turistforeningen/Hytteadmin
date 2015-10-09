2d9a8cd15eea00a447bcec6ad819e1bdab4e6fc5
Håvard Ranum <havard.ranum@me.com>
2015-10-09 10:10:46 +0200

    Added validation rules for foto item

--- client/app/components/dnt-photo-manager-list-item.js
+++ client/app/components/dnt-photo-manager-list-item.js
@@ -6,9 +6,49 @@ export default Ember.Component.extend({
   classNames: ['dnt-photo-manager-list-item', 'item'],
   attributeBindings: ['photo.id:data-id'],
 
+  validationRules: [],
+
   actions: {
     save: function () {
       this.get('photo').save();
     }
-  }
+  },
+
+  updateValidationRules: function () {
+    let newRules = [
+      {
+        identifier: this.get('beskrivelsePath'),
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Bildet må ha en beskrivelse'
+          }
+        ]
+      },
+      {
+        identifier: this.get('fotografEpostPath'),
+        rules: [
+          {
+            type: 'email',
+            prompt: 'Må være en gyldig epostadresse'
+          }
+        ]
+      }
+    ];
+
+    this.get('validationRules').addObjects(newRules);
+
+  }.on('didInsertElement'),
+
+  beskrivelsePath: Ember.computed('photo.id', {
+    get: function () {
+      return ['bilder', this.get('photo.id'), 'beskrivelse'].join('-');
+    }
+  }),
+
+  fotografEpostPath: Ember.computed('photo.id', {
+    get: function () {
+      return ['bilder', this.get('photo.id'), 'fotograf', 'epost'].join('-');
+    }
+  })
 });
