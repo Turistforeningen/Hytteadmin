37b3906ff18fa121f73d143c417af3b1cbc4009b
Håvard Ranum <havard.ranum@me.com>
2015-10-20 21:05:46 +0200

    If input type is number, force value to be number as well

--- client/app/overrides/ember-textfield.js
+++ client/app/overrides/ember-textfield.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.TextField.reopen({
-  attributeBindings: ['data-validate']
+  attributeBindings: ['data-validate'],
+
+  forceNumber: Ember.observer('value', function () {
+    let value = this.get('value');
+    let type = this.get('type');
+
+    if (type  === 'number' && Ember.typeOf(value) !== 'number') {
+      this.set('value', Number(value));
+    }
+  })
+
 });
