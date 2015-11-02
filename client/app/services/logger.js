a46ce8d85da5c8a874c1aa9a6fd8e569c9a00d27
Håvard Ranum <havard.ranum@me.com>
2015-11-02 14:23:18 +0100

    Overwrite empty logger with default generated by ember-cli-raven plugin

--- client/app/services/logger.js
+++ client/app/services/logger.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
+import RavenLogger from 'ember-cli-sentry/services/raven';
 
-export default Ember.Service.extend({
+const { computed } = Ember;
+
+export default RavenLogger.extend({
+
+  unhandledPromiseErrorMessage: '',
+
+  captureException(error) {
+    this._super(...arguments);
+  },
+
+  captureMessage(message) {
+    return this._super(...arguments);
+  },
+
+  enableGlobalErrorCatching() {
+    return this._super(...arguments);
+  }
 });
