0acc5c242910354ebfaf95fa1cb10ed5403186e2
Håvard Ranum <havard.ranum@me.com>
2015-11-02 13:22:50 +0100

    Only include norwegian locales for smaller filesize

--- client/config/environment.js
+++ client/config/environment.js
@@ -27,7 +27,7 @@ module.exports = function(environment) {
       // when it is created
     },
     moment: {
-      includeLocales: true
+      includeLocales: ['nb']
     },
     sentry: {
       skipCdn: false, // Skip loading from CDN
