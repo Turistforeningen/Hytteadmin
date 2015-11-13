8c472ffff463c533fb1060e9ed7772dd89003edc
Håvard Ranum <havard.ranum@me.com>
2015-11-13 11:31:07 +0100

    Added uservoice component

---
+++ client/app/components/uservoice-tab.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  setup: Ember.on('didInsertElement', function () {
+    //
+    // UserVoice Javascript SDK developer documentation:
+    // https://www.uservoice.com/o/javascript-sdk
+    //
+
+  })
+});
+
+
+
