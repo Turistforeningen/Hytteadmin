8d084e8c4b7c260d7af075d32551ed35fb354f96
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-01-05 14:55:04 +0100

    Eslint all the things 🐾

--- server/controllers/turbasen.js
+++ server/controllers/turbasen.js
@@ -1,110 +1,111 @@
-var express = require('express');
+'use strict';
 
-var app = express.Router();
-var auth = require('./auth');
-var helpers = require('../lib/helpers');
+const router = require('express').Router;
+const app = router();
 
-var ntb = require('turbasen');
+const helpers = require('../lib/helpers');
 
-var js = require('JSONStream');
-var es = require('event-stream');
+const ntb = require('turbasen');
 
-ntb.configure({USER_AGENT: 'Hytteadmin/' + require('../package.json').version});
+// const js = require('JSONStream');
+// const es = require('event-stream');
 
-app.use(function(req, res, next) {
+ntb.configure({ USER_AGENT: 'Hytteadmin/' + require('../package.json').version });
+
+app.use(function apiUse(req, res, next) {
   // @TODO validate user authentication
   next();
 });
 
-app.get('/cabin', function(req, res) {
+app.get('/cabin', function apiGetCabins(req, res) {
   req.query.tilbyder = 'DNT';
   req.query['tags.0'] = 'Hytte';
   req.query.sort = 'navn';
   // @TODO set or validate req.query.grupper
 
-  var cabins = ntb.steder(req.query);
+  const cabins = ntb.steder(req.query);
 
   cabins.pipe(res);
-  cabins.pipe(js.parse('documents.*')).pipe(es.mapSync(function(data) {
-    //console.log(data);
-  }));
+  // cabins.pipe(js.parse('documents.*')).pipe(es.mapSync(function esMapSync(data) {
+  //   //console.log(data);
+  // }));
 });
 
-app.post('/cabin', function(req, res) {
+app.post('/cabin', function apiPostCabins(req, res) {
   // @TODO validate user authentication
   req.body = helpers.utify(req.body);
 
   ntb.steder.post(req.body).pipe(res);
 });
 
-app.get('/cabin/:id', function(req, res) {
+app.get('/cabin/:id', function apiGetCabin(req, res) {
   // @TODO validate user authorization
-  var cabin = ntb.steder.get(req.params.id);
+  const cabin = ntb.steder.get(req.params.id);
 
   cabin.pipe(res);
-  cabin.pipe(js.parse()).pipe(es.mapSync(function(data) {
-    //console.log(data);
-  }));
+  // cabin.pipe(js.parse()).pipe(es.mapSync(function esMapSync(data) {
+  //   //console.log(data);
+  // }));
 });
 
-app.put('/cabin/:id', function(req, res) {
+app.put('/cabin/:id', function apiPostCabin(req, res) {
   // @TODO validate user authorization
   req.body = helpers.utify(req.body);
 
   ntb.steder.put(req.params.id, req.body).pipe(res);
 });
 
-app.delete('/cabin/:id', function(req, res) {
+app.delete('/cabin/:id', function apiDeleteCabin(req, res) {
   // @TODO validate user authorization
 
   ntb.steder.delete(req.params.id).pipe(res);
 });
 
-app.post('/photo', function(req, res) {
+app.post('/photo', function apiGetPhotos(req, res) {
   ntb.bilder.post(req.body).pipe(res);
 });
 
-app.get('/photo/:id', function(req, res) {
+app.get('/photo/:id', function apiGetPhoto(req, res) {
   // @TODO validate user authorization
   ntb.bilder.get(req.params.id).pipe(res);
 });
 
-app.post('/photo/:id', function(req, res) {
+app.post('/photo/:id', function apiPostPhoto(req, res) {
   // @TODO validate user authorization
   ntb.bilder.patch(req.params.id, req.body).pipe(res);
 });
 
-app.put('/photo/:id', function(req, res) {
+app.put('/photo/:id', function apiPutPhoto(req, res) {
   // @TODO validate user authorization
   ntb.bilder.put(req.params.id, req.body).pipe(res);
 });
 
-app.get('/group', function(req, res) {
+app.get('/group', function apiGetGrups(req, res) {
   req.query.sort = 'navn';
 
-  var groups = ntb.grupper(req.query);
+  const groups = ntb.grupper(req.query);
 
   groups.pipe(res);
-  groups.pipe(js.parse('documents.*')).pipe(es.mapSync(function(data) {
-    //console.log(data);
-  }));
+  // groups.pipe(js.parse('documents.*')).pipe(es.mapSync(function esMapSync(data) {
+  //   //console.log(data);
+  // }));
 });
 
-app.get('/group/:id', function(req, res) {
+app.get('/group/:id', function apiGetGroup(req, res) {
   // @TODO validate user authorization
   ntb.grupper.get(req.params.id).pipe(res);
 });
 
-app.get('/area', function(req, res) {
-  var areas = ntb.områder(req.query);
+app.get('/area', function apiGetAreas(req, res) {
+  const areas = ntb.områder(req.query);
 
   areas.pipe(res);
-  areas.pipe(js.parse('documents.*')).pipe(es.mapSync(function(data) {
-    //console.log(data);
-  }));
+  // areas.pipe(js.parse('documents.*')).pipe(es.mapSync(function esMapSync(data) {
+  //   //console.log(data);
+  // }));
 });
 
-app.get('/area/:id', function(req, res) {
+app.get('/area/:id', function apiGetArea(req, res) {
   // @TODO validate user authorization
   ntb.områder.get(req.params.id).pipe(res);
 });
--- server/db/redis.js
+++ server/db/redis.js
@@ -1,1 +1,3 @@
+'use strict';
+
 module.exports = require('redis').createClient(6379, 'redis');
